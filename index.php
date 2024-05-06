d4b49968c4e935f9db3c5e1fd979681f3e6d274b
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-06 17:17:53 +0200

    ADD: printed data for each element in the array through a for cycle

--- index.php
+++ index.php
@@ -39,6 +39,40 @@
         ],
 
     ];
-
-    
 ?>
+
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PHP Hotel</title>
+        </head>
+        <body>
+
+        <?php
+        for($i = 0; $i < count($hotels); $i++) {
+            $hotel = $hotels[$i];
+
+            $name = $hotel['name'];
+            $description = $hotel['description'];
+            $hotel['parking'] == true ? $has_parking = "yes" : $has_parking = "no";
+            $vote = $hotel['vote'];
+            $distance_to_center = $hotel['distance_to_center'];
+        ?>
+            
+            <p>
+                <?php echo "Hotel name: $name <br>" ?>
+                <?php echo "Hotel description: $description <br>"?>
+                <?php echo "The hotel has parking: $has_parking <br>"?>
+                <?php echo "The hotel's rating is $vote <br>"?>
+                <?php echo "The hotel's distance to the center is $distance_to_center km <br>"?>
+            </p>
+
+        <?php
+        }
+        ?>
+            
+        </body>
+        </html>
+
